cb72f8530a90f2b8d818eb0b8e8c4c27100458d1
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2023-12-14 19:45:01 +0530

    All done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Machine learning project  
+## MAchine Learning Projects
--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]  :
+def get_requirements(file_path:str)->List[str]:
     requirements=[]
-    with open(file_path) as file_object:
-        requirements=file_object.readlines()
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
         return requirements
 
 
 setup(
-    name="Diamond_prediction",
+    name='DiamondPricePrediction',
     version='0.0.1',
-    author=" Arkaprabha ",
-    author_email="arkaofficial13@gmail.com",
+    author='Krish',
+    author_email='krish.naik@pw.live',
     install_requires=get_requirements('requirements.txt'),
-    packages=find_packages()  
-
+    packages=find_packages()
 
 )
